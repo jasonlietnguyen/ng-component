6a75a23fe87cefd6f8bbe47bb05975bd01723f9b
jasonlietnguyen@gmail.com <jason123nguyen@gmail.com>
2017-06-28 12:54:40 -0600

    add data service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,57 @@
 import { Component } from '@angular/core';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  <h1>Hello</h1>
+  
+  <!--Property Binding
+  <img src="{{logo}}">
+  <img [src]="logo">
+  <img bind-src="logo">-->
+
+  <!--Event Binding
+  <button (mouseover)="myEvent()">My Button</button>-->
+
+  <h1 [ngClass]="titleClasses">Hello!</h1>
+
+  <p>{{foo}}</p>
+
+  `,
+  styles: [`
+  h1{
+    text-decoration: underline;
+  }
+
+  .red{
+    color: red;
+  }
+  .large{
+    font-size: 5em;
+  }
+  `]
 })
 export class AppComponent {
-  title = 'app';
+  constructor(private dataService:DataService){
+
+  }
+
+  foo = '';
+
+  ngOnInit(){
+    console.log(this.dataService.cars)
+    this.foo = this.dataService.myData()
+  }
+
+  name = "jason"
+  myEvent() {
+    console.log(this.name)
+  }
+
+  titleClasses = {
+    'red': true,
+    'large': true
+  }
+
 }
